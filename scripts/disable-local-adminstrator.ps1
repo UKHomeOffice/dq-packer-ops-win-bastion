1a9cfe9d9337e95c2cbe2ddd2e810e2becaba4a2
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-09-11 15:53:20 +0100

    feat: cips-8020 modified to disable local admin

--- scripts/disable-local-adminstrator.ps1
+++ scripts/disable-local-adminstrator.ps1
@@ -1,7 +1,2 @@
-#Step 1: Add the user Administrator to the Users group 
- 
 Add-LocalGroupMember –Group “Users” -Member “Administrator” 
- 
-#Step 2: Remove the user Administrator from the Administrators group: 
- 
 Remove–LocalGroupMember –Group “Administrators” -Member “Administrator” 
