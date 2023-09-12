68374316d387448bbb31751fe420433df317f790
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-09-12 12:48:59 +0100

    feat: yel-8020 modified to fix syntax error

--- scripts/disable-local-adminstrator.ps1
+++ scripts/disable-local-adminstrator.ps1
@@ -2,7 +2,7 @@ function Change-groupadmin {
    Add-LocalGroupMember -Group Users -Member "Administrator"  
 }
 function Disable-Localadmin {   
-   Remove–LocalGroupMember -Group Administrators -Member "Administrator"  
+   Remove–LocalGroupMember -Group "Administrators" -Member "Administrator"  
 }
 Change-groupadmin
 Disable-Localadmin
