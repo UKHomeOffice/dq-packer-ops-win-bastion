44084a3af1cd3ca289eef1685b235bccf37ee3b2
Mustafa Deerow <mdeerow@hodqadms-MacBook-Pro-4.local>
2023-12-13 11:59:33 +0000

    update userdata script

--- scripts/userdata.ps1
+++ scripts/userdata.ps1
@@ -516,15 +516,19 @@ if (-not $sym_flag)
 # Try to figure out the Environment from the IP address
     Write-Host "Deciphering the Environment from subnet part of the IP Address $subnet_part"
     if($subnet_part -eq "10.8"){
-        Write-Host "Notprod symantec client had been copied"
+        Write-Host "Copying the notprod symantec client"
         aws s3 cp s3://s3-dq-ops-config-notprod/symantec-client/DQ_Win_Servers_WIN64BIT/Symantec_Endpoint_Protection_version_14.3.10148.8000/setup.exe C:\tmp\setup.exe
-        Start-Process "C:\tmp\setup.exe" "powershell" -Verb RunAs
+        Write-Host "Installing notprod symantec client"
+        Start-Process "C:\tmp\setup.exe" "powershell" -Verb RunAs -Wait
+        Write-Host "Restarting machine"
         Restart-Computer -Force
     }
     elseif($subnet_part -eq "10.2"){
-        Write-Host "Prod symantec client had been copied"
+        Write-Host "Copying the prod symantec client"
         aws s3 cp s3://s3-dq-ops-config-prod/symantec-client/DQ_Win_Servers_WIN64BIT/Symantec_Endpoint_Protection_version_14.3.10148.8000/setup.exe C:\tmp\setup.exe
-        Start-Process "C:\tmp\setup.exe" "powershell" -Verb RunAs
+        Write-Host "Installing prod symantec client"
+        Start-Process "C:\tmp\setup.exe" "powershell" -Verb RunAs -Wait
+        Write-Host "Restarting machine"
         Restart-Computer -Force
     }
     else{
